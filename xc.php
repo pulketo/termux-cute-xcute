9917430fdb558c5e1a0c0a739d08698c09181ae7
mario.martinez <mario.martinez@seccionamarilla.com>
2018-09-03 17:50:20 -0500

    encoding fix

--- xc.php
+++ xc.php
@@ -4,12 +4,18 @@
 			This is a better launcher search by keywords autocompletable, but it fully depends/relies on Termux-Launcher (https://github.com/amsitlab/termuxlauncher) 
 			which generates a file on: 			/sdcard/termuxlauncher/.apps-launcher, and it will only work with this file.
 		*/
-		DEFINE("VERSION", "v0.93");
+		DEFINE("VERSION", "v0.94");
 		DEFINE("MAN", 
 	"
 	MANUAL PAGES
 	");
 	require __DIR__ ."/vendor/autoload.php";
+
+	function fixEncoding($i){
+		$t = array(
+		  "\u00c0" =>"À",     "\u00c1" =>"Á",     "\u00c2" =>"Â",     "\u00c3" =>"Ã",     "\u00c4" =>"Ä",     "\u00c5" =>"Å",     "\u00c6" =>"Æ",     "\u00c7" =>"Ç",     "\u00c8" =>"È",     "\u00c9" =>"É",     "\u00ca" =>"Ê",     "\u00cb" =>"Ë",     "\u00cc" =>"Ì",     "\u00cd" =>"Í",     "\u00ce" =>"Î",     "\u00cf" =>"Ï",     "\u00d1" =>"Ñ",     "\u00d2" =>"Ò",     "\u00d3" =>"Ó",     "\u00d4" =>"Ô",     "\u00d5" =>"Õ",     "\u00d6" =>"Ö",     "\u00d8" =>"Ø",     "\u00d9" =>"Ù",     "\u00da" =>"Ú",     "\u00db" =>"Û",     "\u00dc" =>"Ü",     "\u00dd" =>"Ý",     "\u00df" =>"ß",     "\u00e0" =>"à",     "\u00e1" =>"á",     "\u00e2" =>"â",     "\u00e3" =>"ã",     "\u00e4" =>"ä",     "\u00e5" =>"å",     "\u00e6" =>"æ",     "\u00e7" =>"ç",     "\u00e8" =>"è",     "\u00e9" =>"é",     "\u00ea" =>"ê",     "\u00eb" =>"ë",     "\u00ec" =>"ì",     "\u00ed" =>"í",     "\u00ee" =>"î",     "\u00ef" =>"ï",     "\u00f0" =>"ð",     "\u00f1" =>"ñ",     "\u00f2" =>"ò",     "\u00f3" =>"ó",     "\u00f4" =>"ô",     "\u00f5" =>"õ",     "\u00f6" =>"ö",     "\u00f8" =>"ø",     "\u00f9" =>"ù",     "\u00fa" =>"ú",     "\u00fb" =>"û",     "\u00fc" =>"ü",     "\u00fd" =>"ý", "\u00ff" =>"ÿ");
+		return strtr($i, $t);
+	}
 
 	function arrSearch($hayStack, $needle){
 		$o = array_filter($hayStack, function($el) use ($needle) {
@@ -43,6 +49,12 @@
 		return $txt;
 		
 	}
+	function json_enc_show($arr){
+		$o = json_encode($arr, JSON_PRETTY_PRINT);
+		$o = fixEncoding($o);
+		echo $o;
+	}
+
 	/**main**/
 	$appsInfo=getAppsInfo();
 
@@ -65,7 +77,7 @@
 	// No arguments
 	if ($opts['list'] || $noargs ){
 		fwrite(STDERR, "showing app list".PHP_EOL);
-		echo json_encode($appsInfo, JSON_PRETTY_PRINT);
+		json_enc_show($appsInfo);
 		exit(0);
 	}
 	// check script arguments maximum 15 arguments...
@@ -96,7 +108,7 @@
 			foreach($response as $k=>$v){
 				$o['status'.$k] = $v;
 			}
-			echo json_encode($o, JSON_PRETTY_PRINT).PHP_EOL;
+			json_enc_show($o, JSON_PRETTY_PRINT).PHP_EOL;
 			break;
 		default:
 			// more than one match
@@ -112,9 +124,9 @@
   			foreach($response as $k=>$v){
 				  $o['status'.$k] = $v;
 			  }
-				echo json_encode($o, JSON_PRETTY_PRINT).PHP_EOL;
+				json_enc_show($o, JSON_PRETTY_PRINT).PHP_EOL;
 			}else{
-				echo json_encode($o, JSON_PRETTY_PRINT).PHP_EOL;
+				json_enc_show($o, JSON_PRETTY_PRINT).PHP_EOL;
 				fwrite(STDERR, "Multiple matches found, but -f not specified".PHP_EOL);
 			}
 			break;
