19b3f9e094c84bb73f19d35761f532d62e9f6559
pulketo <pulketo@gmail.com>
2018-09-07 12:50:39 -0500

    install.sh script add

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cp xc.phar /data/data/com.termux/files/usr/bin/xc.phar
 cd /data/data/com.termux/files/usr/bin/
+chmod +x xc.phar
 ln -s xc.phar x && echo "launch with x whats or x --help"
