889a27bb74ca13563ccd2c1618ba2ae26cc51c70
pulketo <pulketo@gmail.com>
2018-09-07 12:46:16 -0500

    install.sh script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cp xc.phar /data/data/com.termux/files/usr/bin/xc.phar
+cd /data/data/com.termux/files/usr/bin/
+ln -s xc.phar x && echo "launch with x whats or x --help"
